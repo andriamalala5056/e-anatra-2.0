dc6db3bc330baf4a4a66a898a4f2a1aac93c82cc
Andryhajanirina <andryhaj@gmail.com>
2018-06-25 08:24:37 +0300

    Réinitialisation bdd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'localhost',
-    port:                 1025, }
+    port:                 1025
+  }
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   address:              'smtp.gmail.com',
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,6 @@
 # filiere5 = Filiere.create(filiere_name: 'Medecine')
 # filiere6 = Filiere.create(filiere_name: 'Autre')
 
-# newsletter1 = Newsletter.create(lastname: 'ANDRIANAVALONA', firstname:'Andry', email: 'andry@gmail.com')
+# newsletter1 = Newsletter.create(lastname: 'ANDRIANAVALONA', firstname:'Andry', email: 'eanatramada@gmail.com')
 # newsletter2 = Newsletter.create(lastname: 'ANDRIANAVALONA', firstname:'Kanto', email: 'kanto@gmail.com')
 # newsletter3 = Newsletter.create(lastname: 'ANDRIANAVALONA', firstname:'Fetra', email: 'fetra@gmail.com')
