0bff9647a34fc3e5d70730ac7d6089cf1eae73d1
Andryhajanirina <andryhaj@gmail.com>
2018-06-28 14:27:59 +0300

    Mailing

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ApplicationMailer
     default from: 'andryhaj@gmail.com'
     def contact(destinataire, message)
+        # attachments['free_book.pdf'] = File.read('path/to/file.pdf')
         @destinataire = destinataire
         @message = message
         # mail :subject => "#{message[:subject]} from: #{message[:name]}",
